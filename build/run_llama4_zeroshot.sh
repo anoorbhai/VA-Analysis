627f1fc83807589152bcb083b7147054045d1807
anoorbhai <anoorbhai@icloud.com>
2025-10-04 00:32:07 +0200

    edited bash script

--- build/run_llama4_zeroshot.sh
+++ build/run_llama4_zeroshot.sh
@@ -3,11 +3,12 @@
 #SBATCH --output=llama4_va_%j.out
 #SBATCH --error=llama4_va_%j.err
 #SBATCH --time=7-00:00:00
+#SBATCH --reservation=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32G
 
 # Change to script directory
 cd /home/noorbhaia/VA-Analysis
-
-srun --reservation gpu --gres=gpu:1 --pty bash
 
 # Run the Python script
 python Prompting/Llama4_ZeroShot.py
