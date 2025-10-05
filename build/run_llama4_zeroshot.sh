06e99698d5853179e79d4137ed2c4d8aac8f5349
anoorbhai <anoorbhai@icloud.com>
2025-10-05 19:27:00 +0200

    change loc of log files

--- build/run_llama4_zeroshot.sh
+++ build/run_llama4_zeroshot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=llama4_va
-#SBATCH --output=llama4_va_%j.out
-#SBATCH --error=llama4_va_%j.err
+#SBATCH --output=../logs/llama4_va_%j.out
+#SBATCH --error=../logs/llama4_va_%j.err
 #SBATCH --time=7-00:00:00
 #SBATCH --reservation=gpu
 #SBATCH --gres=gpu:1
