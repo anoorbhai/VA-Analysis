91ba38495135319bca6b135cacf094849446a0d5
anoorbhai <anoorbhai@icloud.com>
2025-10-11 14:38:53 +0200

    Added llama 3 70b clinician COD List code

--- Prompting/ZeroShot/Llama3_8b_ZeroShot_COD.py
+++ Prompting/ZeroShot/Llama3_8b_ZeroShot_COD.py
@@ -23,7 +23,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler(f'llama3_zeroshot_COD_{log_timestamp}.log'),
+        logging.FileHandler(f'llama3_70b_zeroshot_COD_{log_timestamp}.log'),
         logging.StreamHandler()
     ]
 )
@@ -33,10 +33,9 @@ logger = logging.getLogger(__name__)
 INPUT_CSV_PATH = "/dataA/madiva/va/student/madiva_va_dataset_20250924.csv"
 # Generate timestamped output filename
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-OUTPUT_CSV_PATH = f"/spaces/25G05/CODlist/llama3_zeroshot_COD_results_{timestamp}.csv"
+OUTPUT_CSV_PATH = f"/spaces/25G05/Aaliyah/ZeroShot/llama3_70b_zeroshot_COD_results_{timestamp}.csv"
 OLLAMA_API_URL = "http://localhost:11434/api/generate"
-MODEL_NAME = "Llama3_8b_zero_COD:latest" 
-
+MODEL_NAME = "llama3_70b_VA_COD:latest"
 # Fields to exclude as specified
 EXCLUDE_FIELDS = ['cause1', 'prob1', 'cause2', 'prob2', 'cause3', 'prob3']
 
