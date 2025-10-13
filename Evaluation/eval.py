1a429a923d5534f5c7da18f2fe44f7b950e4d7c6
Rizwaanah <riziwaanah1808@gmail.com>
2025-10-13 12:09:43 +0200

    adjusted results csv path

--- Evaluation/eval.py
+++ Evaluation/eval.py
@@ -7,13 +7,13 @@ from typing import Optional, List
 # ---------- CONFIG ----------
 # Filtering options
 REMOVE_MISSING_ICD10 = True    # Remove entries with missing/empty clinician ICD-10 codes
-REMOVE_R99_CODES = True        # Remove entries with R99 (ill-defined causes) codes
+REMOVE_R99_CODES = False        # Remove entries with R99 (ill-defined causes) codes
 # REMOVE_R99_CODES = True        # Remove entries with R99 (ill-defined causes) codes
 
 LOG_FILE = "/spaces/25G05/CODlist/evaluation_log.txt"
 LLM_RESULTS_CSV = "/spaces/25G05/CODlist/llama3_8b_fewshot_COD_results_20251013_113000.csv" 
 LOG_FILE = "/spaces/25G05/CODlist/evaluation_log.txt"
-LLM_RESULTS_CSV = "/spaces/25G05/CODlist/llama3_8b_fewshot_COD_results_20251008_203255.csv" 
+# LLM_RESULTS_CSV = "/spaces/25G05/CODlist/llama3_8b_fewshot_COD_results_20251008_203255.csv" 
 CLINICIAN_CSV   = "/dataA/madiva/va/student/madiva_va_clinician_COD_20250926.csv"
 
 # Generate output filename with current date and time
