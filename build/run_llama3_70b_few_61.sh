84220b74fc8a5acf07bcfa7f76ff37bd15e156d1
anoorbhai <anoorbhai@icloud.com>
2025-10-17 15:12:52 +0200

    70 b 61 cuases few shot

--- build/run_llama3_70b_few_61.sh
+++ build/run_llama3_70b_few_61.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=llama3
-#SBATCH --output=logs/llama3_70b_61_cods%j.out
-#SBATCH --error=logs/llama3_70b_61_cods%j.err
+#SBATCH --output=logs/llama3_70b_cods%j.out
+#SBATCH --error=logs/llama3_70b_cods%j.err
 #SBATCH --time=7-00:00:00
 #SBATCH --reservation=gpu
+#SBATCH --nodelist=n22
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32G
 
