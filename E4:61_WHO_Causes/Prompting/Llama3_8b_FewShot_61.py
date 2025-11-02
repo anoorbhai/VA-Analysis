8c08cef0b75a55d95275ae5d4dc48675ee9a0c3a
Rizwaanah <riziwaanah1808@gmail.com>
2025-11-02 20:26:07 +0200

    renamed 61 casues model

--- E4:61_WHO_Causes/Prompting/Llama3_8b_FewShot_61.py
+++ E4:61_WHO_Causes/Prompting/Llama3_8b_FewShot_61.py
@@ -27,7 +27,7 @@ INPUT_CSV_PATH = "/dataA/madiva/va/student/madiva_va_dataset_20250924.csv"
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 OUTPUT_CSV_PATH = f"/spaces/25G05/FewShot/llama3_8b_fewshot_61_results_{timestamp}.csv"
 OLLAMA_API_URL = "http://localhost:11434/api/generate"
-MODEL_NAME = "Llama3-8b-Few-61:latest" 
+MODEL_NAME = "Llama3_8b_Few_61:latest" 
 
 # Exclude InsilicoVA predictions
 EXCLUDE_FIELDS = ['cause1', 'prob1', 'cause2', 'prob2', 'cause3', 'prob3']
